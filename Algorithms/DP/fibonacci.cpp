08732d4a1cae8dd07c3f78b17e2f0beec0d9c98a
Chungmin Park(Chuck) <chuck.m.park@gmail.com>
2019-01-14 10:06:45 +0900

    adding loop solution in fibonacci

--- Algorithms/DP/fibonacci.cpp
+++ Algorithms/DP/fibonacci.cpp
@@ -6,12 +6,25 @@ using namespace std;
 
 vector<int> dp;
 
-int fibonacci(int n) {
+// recursive
+int fibonacci_1(int n) {
     if (n == 0) return 0;
     if (n == 1) return 1;
     if(dp[n] != -1) return dp[n];
-    dp[n] = fibonacci(n-2) + fibonacci(n-1);
+    dp[n] = fibonacci_1(n-2) + fibonacci_1(n-1);
     return dp[n];
+}
+
+// loop
+int fibonacci_2(int n) {
+    vector<int> dp2(n+1, 0);
+    dp2[1] = 1;
+    if (n == 0) return dp2[0];
+    if (n == 1) return dp2[1];
+    for (int i=2; i<n+1; i++) {
+        dp2[i] = dp2[i-2] + dp2[i-1];
+    }
+    return dp2[n];
 }
 
 int main() {
@@ -19,7 +32,7 @@ int main() {
     int N;
     scanf("%d", &N);
     dp.resize(N+1, -1);
-    printf("%d", fibonacci(N));
+    printf("%d", fibonacci_2(N));
 
     return 0;
 }
